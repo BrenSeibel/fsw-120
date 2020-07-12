ea6188722afd1f9634bcce57679b3e6ee1c85cbb
BrenSeibel <brens.seibel@gmail.com>
2020-07-12 14:16:46 -0400

    working

---
+++ week6/calculator/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+import ResultComponent from './ResultComponent';
+import KeyPadComponent from "./KeyPadComponent";
+
+class App extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            result: ""
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="calculator-body">
+                    <h1>Simple Calculator</h1>
+                    <ResultComponent result={this.state.result}/>
+                    <KeyPadComponent onClick={this.onClick}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
