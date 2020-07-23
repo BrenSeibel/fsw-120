4cec3f69568412c08560f35d8f87f8bb53bbe0ba
BrenSeibel <brens.seibel@gmail.com>
2020-07-23 00:57:03 -0400

    working

--- week7/twitter/twitter/src/Home.js
+++ week7/twitter/twitter/src/Home.js
@@ -2,14 +2,13 @@
 import React from 'react';
 import './App.css'
 import TweetsList from './TweetsList.js';
-import {withAuth as WithAuth} from './AuthProvider'
+import WithAuth from './AuthProvider';
 
 const Home = () => {
   return (
     <div>
       <h1> Home </h1>
-      <TweetsList />
-    <WithAuth> loop </WithAuth>
+      <TweetsList/>
     </div>
   );
 
